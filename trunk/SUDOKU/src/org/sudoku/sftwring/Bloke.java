faba96c19cf1e4b8dcfe370e6293499ac2dccc24
barakataka666 <barakataka666@ed341c4f-5e1b-f16c-1c88-a53bf4868cfa>
2015-02-23 15:45:13 +0000

    Blokea aldatu dut baina ez dakit nola hasieratu, bota begirada bat

--- trunk/SUDOKU/src/org/sudoku/sftwring/Bloke.java
+++ trunk/SUDOKU/src/org/sudoku/sftwring/Bloke.java
@@ -1,34 +1,50 @@
 package org.sudoku.sftwring;
 
 public class Bloke {
-Kasila [] kasilaZerrenda= new Kasila [9];
+	
 
+Bloke() {
+	Kasila [][] bloke= new Kasila [3][3];//nola hasieratzen da hau?
+  }
+ 
+  Bloke(Kasila[][] bloke) {
+ for (int i=0;i<3;i++){
+	 for (int j=0;j<3;j++) {
+	bloke[i][j]=new Kasila();	 
+	 }
+ }
+  }
 	public static void main(String[] args) {
-Bloke b =new Bloke();
-
-b.kasilaZerrenda[0]=new Kasila();
-b.kasilaZerrenda[1]=new Kasila();
-b.kasilaZerrenda[2]=new Kasila();
-b.kasilaZerrenda[3]=new Kasila();
-b.kasilaZerrenda[4]=new Kasila();
-b.kasilaZerrenda[5]=new Kasila();
-b.kasilaZerrenda[6]=new Kasila();
-b.kasilaZerrenda[7]=new Kasila();
-b.kasilaZerrenda[8]=new Kasila();
-b.inprimatuBloke();
-b.aldatuKasila_(0, 1);
-b.aldatuKasila_(4, 1);
-b.aldatuKasila_(8, 1);
+Bloke b=new Bloke();
+Bloke.inprimatuBloke();
+//b.bloke[0][0]=new Kasila();
+//b.bloke[0][1]=new Kasila();
+//b.bloke[0][2]=new Kasila();
+//b.bloke[1][0]=new Kasila();
+//b.bloke[1][1]=new Kasila();
+//b.bloke[1][2]=new Kasila();
+//b.bloke[2][0]=new Kasila();
+//b.bloke[2][1]=new Kasila();
+//b.bloke[2][2]=new Kasila();
+//inprimatuBloke(b);
+b.aldatuKasila_(b,0,0, 1);
+b.aldatuKasila_(b,1,2, 1);
+b.aldatuKasila_(b,2,2, 1);
 b.inprimatuBloke();
 	}
+private static void inprimatuBloke() {
+		// TODO Auto-generated method stub
+		
+	}
+
 public Bloke inprimatuBloke(){
-	System.out.println(this.kasilaZerrenda[0].balioa+"   "+this.kasilaZerrenda[1].balioa+"   "+this.kasilaZerrenda[2].balioa+"   "+"\n");	
-	System.out.println(this.kasilaZerrenda[3].balioa+"   "+this.kasilaZerrenda[4].balioa+"   "+this.kasilaZerrenda[5].balioa+"   "+"\n");
-	System.out.println(this.kasilaZerrenda[6].balioa+"   "+this.kasilaZerrenda[7].balioa+"   "+this.kasilaZerrenda[8].balioa+"   "+"\n");
+	System.out.println(bloke[0][0].balioa+"   "+bloke[0][1].balioa+"   "+bloke[0][2].balioa+"   "+"\n");	
+	System.out.println(bloke[1][0].balioa+"   "+bloke[1][1].balioa+"   "+bloke[1][2].balioa+"   "+"\n");
+	System.out.println(bloke[2][0].balioa+"   "+bloke[2][1].balioa+"   "+bloke[2][2].balioa+"   "+"\n");
 	return this;
 	}
-public void aldatuKasila_(int pInd,int pBalioBerria){
-	this.kasilaZerrenda[pInd].balioa=pBalioBerria;
+public void aldatuKasila_(Bloke b,int pIndX,int pIndY,int pBalioBerria){
+	b[pIndX][pIndX].balioa=pBalioBerria;
 }
 
 }
