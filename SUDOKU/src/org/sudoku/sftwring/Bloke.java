f0fb192422f03b7050431c4ea591ba24b378908d
bazabal1995 <bazabal1995@ed341c4f-5e1b-f16c-1c88-a53bf4868cfa>
2015-02-24 12:55:12 +0000

    Blokea klaseko erroreak konpondu ditut baina sudokuak baditu oraindik erroreak, PorbatzekoMain bat sortu dut probak egin behar dituenak begiratu mesedez

--- SUDOKU/src/org/sudoku/sftwring/Bloke.java
+++ SUDOKU/src/org/sudoku/sftwring/Bloke.java
@@ -2,50 +2,27 @@ package org.sudoku.sftwring;
 
 public class Bloke {
 	
+	private Kasila [][] bloke;
 
-Bloke() {
-	Kasila [][] bloke= new Kasila [3][3];//nola hasieratzen da hau?
-  }
- 
-  Bloke(Kasila[][] bloke) {
- for (int i=0;i<3;i++){
-	 for (int j=0;j<3;j++) {
-	bloke[i][j]=new Kasila();	 
-	 }
- }
-  }
-	public static void main(String[] args) {
-Bloke b=new Bloke();
-Bloke.inprimatuBloke();
-//b.bloke[0][0]=new Kasila();
-//b.bloke[0][1]=new Kasila();
-//b.bloke[0][2]=new Kasila();
-//b.bloke[1][0]=new Kasila();
-//b.bloke[1][1]=new Kasila();
-//b.bloke[1][2]=new Kasila();
-//b.bloke[2][0]=new Kasila();
-//b.bloke[2][1]=new Kasila();
-//b.bloke[2][2]=new Kasila();
-//inprimatuBloke(b);
-b.aldatuKasila_(b,0,0, 1);
-b.aldatuKasila_(b,1,2, 1);
-b.aldatuKasila_(b,2,2, 1);
-b.inprimatuBloke();
+	public Bloke() {
+		bloke = new Kasila[3][3];
+		for (int i=0;i<3;i++){
+			for (int j=0;j<3;j++) {
+				bloke[i][j]=new Kasila();	 
+			}
+		}
 	}
-private static void inprimatuBloke() {
-		// TODO Auto-generated method stub
-		
+	
+	
+	public Bloke inprimatuBloke(){
+		System.out.println(bloke[0][0].balioa+"   "+bloke[0][1].balioa+"   "+bloke[0][2].balioa+"   "+"\n");	
+		System.out.println(bloke[1][0].balioa+"   "+bloke[1][1].balioa+"   "+bloke[1][2].balioa+"   "+"\n");
+		System.out.println(bloke[2][0].balioa+"   "+bloke[2][1].balioa+"   "+bloke[2][2].balioa+"   "+"\n");
+		return this;
 	}
-
-public Bloke inprimatuBloke(){
-	System.out.println(bloke[0][0].balioa+"   "+bloke[0][1].balioa+"   "+bloke[0][2].balioa+"   "+"\n");	
-	System.out.println(bloke[1][0].balioa+"   "+bloke[1][1].balioa+"   "+bloke[1][2].balioa+"   "+"\n");
-	System.out.println(bloke[2][0].balioa+"   "+bloke[2][1].balioa+"   "+bloke[2][2].balioa+"   "+"\n");
-	return this;
+	public void aldatuKasila_(int pIndX,int pIndY,int pBalioBerria){
+		bloke[pIndX][pIndX].balioa=pBalioBerria;
 	}
-public void aldatuKasila_(Bloke b,int pIndX,int pIndY,int pBalioBerria){
-	b[pIndX][pIndX].balioa=pBalioBerria;
-}
 
 }
 
---
+++ SUDOKU/src/org/sudoku/sftwring/ProbatzekoMain.java
@@ -0,0 +1,19 @@
+package org.sudoku.sftwring;
+
+public class ProbatzekoMain {
+
+	public static void main(String[] args) {
+		
+		blokearenProba();
+	}
+	
+	private static void blokearenProba() {
+		Bloke b=new Bloke();
+		b.aldatuKasila_(0,0, 1);
+		b.aldatuKasila_(1,2, 1);
+		b.aldatuKasila_(2,2, 1);
+		b.inprimatuBloke();
+
+	}
+
+}
