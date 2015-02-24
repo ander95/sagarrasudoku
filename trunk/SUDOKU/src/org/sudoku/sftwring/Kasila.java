3662b7f400bee379702b2c7b77fdb6f01c517e48
bazabal1995 <bazabal1995@ed341c4f-5e1b-f16c-1c88-a53bf4868cfa>
2015-02-24 12:55:12 +0000

    Blokea klaseko erroreak konpondu ditut baina sudokuak baditu oraindik erroreak, PorbatzekoMain bat sortu dut probak egin behar dituenak begiratu mesedez

--- trunk/SUDOKU/src/org/sudoku/sftwring/Kasila.java
+++ trunk/SUDOKU/src/org/sudoku/sftwring/Kasila.java
@@ -1,23 +1,23 @@
 package org.sudoku.sftwring;
 public class Kasila{
-int balioa=0;	
-public static void main(String[] args){	
-Kasila k= new Kasila();
-k.balioa=0;
-k.inprimatuKasila();
-k.aldatuBalioa(9);
-k.inprimatuKasila();
-}
-private void inprimatuKasila(){
-System.out.println(balioa);	
-}
+	int balioa=0;	
+	public static void main(String[] args){	
+		Kasila k= new Kasila();
+		k.balioa=0;
+		k.inprimatuKasila();
+		k.aldatuBalioa(9);
+		k.inprimatuKasila();
+	}
+	private void inprimatuKasila(){
+		System.out.println(balioa);	
+	}
 
-private void aldatuBalioa(int pBalioa){
-this.balioa=pBalioa;	
-}
-private void erreseteatuKasila (Kasila pKasila){
-	pKasila.balioa=0;
-}
+	private void aldatuBalioa(int pBalioa){
+		this.balioa=pBalioa;	
+	}
+	private void erreseteatuKasila (Kasila pKasila){
+		pKasila.balioa=0;
+	}
 }
 
 
