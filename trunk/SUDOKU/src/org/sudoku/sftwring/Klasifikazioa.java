6c673f42069c46199c3057e8d930c9788aa68f13
bazabal1995@gmail.com <bazabal1995@gmail.com@ed341c4f-5e1b-f16c-1c88-a53bf4868cfa>
2015-03-12 12:59:50 +0000

    Anderrek Igo nahi zuena duena eta klasifikazioaren lehioa

--- trunk/SUDOKU/src/org/sudoku/sftwring/Klasifikazioa.java
+++ trunk/SUDOKU/src/org/sudoku/sftwring/Klasifikazioa.java
@@ -2,8 +2,66 @@ package org.sudoku.sftwring;
 
 import java.util.AbstractCollection;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Klasifikazioa {
-
+	private AbstractCollection<Integer> ranking;
+	private Klasifikazioa(){
+		this.ranking = new ArrayList<Integer>();
+	}
+	
+	public void add(int id){
+//	aurre:
+//	post: erabiltzailea zerrendan ez badago gehitu egingo du segituan ordenatzeko
+		if (this.bilatu(id)==-1){
+			ranking.add(id);
+			this.eguneratu();
+		}
+	}
+	
+	public void eguneratu(){
+//	aurre:
+//	post: rankingeko erabiltzaileak beheranzko ordenean ordenatuko ditu puntuazioan oinarrituz
+		
+	}
+	
+	private Iterator<Integer> getIteradorea(){
+		return ranking.iterator();
+	}
+	
+	public int bilatu(int id){
+//	aurre:
+//	post: erabiltzailearen posizioa itzuliko du eta -1 erabiltzailea ez bada zerrendan aurkitzen
+		int pos = 1;
+		boolean aurkitua = false;
+		Iterator<Integer> itr = this.getIteradorea();
+		while(itr.hasNext() || !aurkitua){
+			if ((int)itr.next()==id){
+				aurkitua=true;
+				return pos;
+			}
+			else{
+				pos++;
+			}
+		}
+		return -1;
+	}
+	
+	public void erreseteatu(){
+		this.ranking = new ArrayList<Integer>();
+	}
+	
+	public void inprimatuKlasifikazioa(){
+//		aurre: 
+//		post: Erabiltzaileen izena eta puntuazioa itzuliko du, beherazko ordenean
+		Iterator<Integer> itr = this.getIteradorea();
+		int pos = 1;
+		while(itr.hasNext()){
+			int erabID=(int)itr.next();
+			Erabiltzaile unekoa = ErabiltzaileLista.getErabiltzaileLista().getErabiltzaile(erabID);
+			System.out.println(pos + unekoa.getIzen() + unekoa.getPuntuazioa());
+			pos++;
+		}
+	}
 
 }
