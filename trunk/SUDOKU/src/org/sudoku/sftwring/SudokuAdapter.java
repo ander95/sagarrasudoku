1d7702a5d6bbc48afbf5e72425e0b94475b77748
uribarri95 <uribarri95@ed341c4f-5e1b-f16c-1c88-a53bf4868cfa>
2015-05-03 16:16:52 +0000

    aldaketak generadorearen inguruko klaseekin eta soluzio bakarra lortzeko metodoarekin aurrerapenak

--- trunk/SUDOKU/src/org/sudoku/sftwring/SudokuAdapter.java
+++ trunk/SUDOKU/src/org/sudoku/sftwring/SudokuAdapter.java
@@ -8,6 +8,7 @@ public class SudokuAdapter extends Sudokua {
 
 	public void ausazBete() {
 		GeneradoreSudoku genSudo = new GeneradoreSudoku();
+		//zailtasuna hemen nonbaiten doa generadoreko zenbakiak kentzeko metodoak 0, 1 edo 2 zenbalia beharko duelako
 		genSudo.beteAusaz();
 		pasatu(genSudo);
 	}
