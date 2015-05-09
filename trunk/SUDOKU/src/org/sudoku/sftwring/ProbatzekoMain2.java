63ec043034e18cfa45f84b6266e9916a43448908
barakataka666 <barakataka666@ed341c4f-5e1b-f16c-1c88-a53bf4868cfa>
2015-05-09 13:54:07 +0000

    Zailtazuna era egokian aukeratzen baden begiratzen dituzten frogak

--- trunk/SUDOKU/src/org/sudoku/sftwring/ProbatzekoMain2.java
+++ trunk/SUDOKU/src/org/sudoku/sftwring/ProbatzekoMain2.java
@@ -64,9 +64,73 @@ public class ProbatzekoMain2 {
 	}
 
 	private static void aukeratuLeihoaFrogak() {
-		 System.out.println("Aukeratu leihoa ondo dabil?\n---------------------------");
+		System.out.println("///////////////////////////////////////////////////////////////////////////////////////////////////");
+		System.out.println("Aukeratu leihoaren metodoen frogak:\n----------------------------------");
 		
+		klasifikazioaFrogak();
+		generadoreSudokuFrogak();
+		zailtasunaFrogak();
+		System.out.println("///////////////////////////////////////////////////////////////////////////////////////////////////");
 	}
+
+	private static void zailtasunaFrogak() {
+
+	System.out.println("- Zailtasun bat aukeratuta beharrezko kasilak kentzen dira?");
+	int kontErraza=0;
+	int kontErtaina=0;
+	int kontZaila=0;
+	Sudokua sErraza = new SudokuAdapter();
+	Sudokua sErtaina = new SudokuAdapter();
+	Sudokua sZaila = new SudokuAdapter();
+	sErraza.ausazBete(0);
+	sErtaina.ausazBete(1);
+	sZaila.ausazBete(2);
+	
+	
+	for (int f = 0; f < 9; f++) {
+		for (int i = 0; i < 9; i++) {
+			if(sErraza.getKasila(f, i).getErabiltzaileBal()==0){
+				kontErraza++;
+			}
+		}
+	}
+	for (int f = 0; f < 9; f++) {
+		for (int i = 0; i < 9; i++) {
+			if(sErtaina.getKasila(f, i).getErabiltzaileBal()==0){
+				kontErtaina++;
+			}
+		}
+	}
+	for (int f = 0; f < 9; f++) {
+		for (int i = 0; i < 9; i++) {
+			if(sZaila.getKasila(f, i).getErabiltzaileBal()==0){
+				kontZaila++;
+			}
+		}
+	}
+	System.out.print("Sudoku errezari kendutako kasila kopurua: "+kontErraza+" ");
+	if (kontErraza>=44&&kontErraza<=48){
+		System.out.println("Erraza...OK");
+	}
+	else
+		System.out.println("Erraza...ERROR");
+	System.out.print("Sudoku ertainari kendutako kasila kopurua: "+kontErtaina+" ");
+	if (kontErtaina>=48&&kontErtaina<=52){
+		System.out.println("Ertaina...OK");
+	}
+	else
+		System.out.println("Ertaina...ERROR");
+	System.out.print("Sudoku zailari kendutako kasila kopurua: "+kontZaila+" ");
+
+
+	if (kontZaila>=52&&kontZaila<=56){
+		System.out.println("Zaila...OK");
+	}
+	else
+		System.out.println("Zaila...ERROR");
+	}
+	
+	
 
 	private static void adabegiFrogak() {
 		System.out.println("Adabegi klasearen metodoen frogak:\n----------------------------------");
@@ -104,7 +168,7 @@ public class ProbatzekoMain2 {
 	}
 
 	private static void adabegiaSortuFrogak() {
-		System.out.print("- Adabegia berria zara? ");
+		System.out.print("- Adabegi berria zara? ");
 		Adabegia<String> ada=new Adabegia<String>("Adabegi berria naiz", 0);
 		if (ada.content.equals("Adabegi berria naiz"))
 		System.out.println(ada.content+"\nSortu...OK");
