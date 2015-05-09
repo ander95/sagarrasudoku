3c457be2ff70d6de7f3658583239e850e6bd8cc7
barakataka666 <barakataka666@ed341c4f-5e1b-f16c-1c88-a53bf4868cfa>
2015-05-09 11:57:28 +0000

    Frogatu beharreko metodoen definizioa eta lehengo froga kasuen inplementazioa

--- SUDOKU/src/org/sudoku/sftwring/ProbatzekoMain2.java
+++ SUDOKU/src/org/sudoku/sftwring/ProbatzekoMain2.java
@@ -7,8 +7,92 @@ public class ProbatzekoMain2 {
 
 	public static void main(String[] args) {
 		sudokuaAusazBete();
-		//		proba();
+				//proba();
+		frogaBerriak();
 
+	}
+
+	private static void frogaBerriak() {
+		adabegiFrogak();
+		aukeratuLeihoaFrogak();
+		erregistroLehioaFrogak();
+		generadoreSudokuFrogak();
+		klasifikazioaFrogak();
+		klasifikazioaLeihoaFrogak();
+		laguntzaFrogak();
+		sudokuAdapterFrogak();
+		sudokuSolverFrogak();
+		zifraFrogak();
+		
+	}
+
+	private static void zifraFrogak() {
+		System.out.println("Datuak ondo zifratzen dira?\n---------------------------");
+							
+	}
+
+	private static void sudokuSolverFrogak() {
+		System.out.println("Beti soluzio berdina (bakarra) bueltatzen da?\n-----------------------------------------------"); 
+							
+	}
+
+	private static void sudokuAdapterFrogak() {
+		System.out.println("Patroia era zuzenenan funtzionatzen du?\n---------------------------------------");   
+							
+	}
+
+	private static void laguntzaFrogak() {
+		System.out.println("Laguntza behar bezala funtzionatzen du?\n---------------------------------------");
+							
+	}
+
+	private static void klasifikazioaLeihoaFrogak() {
+		System.out.println("Klasifikazio leihoaren frogak:\n------------------------------");  	
+	}
+
+	private static void klasifikazioaFrogak() {
+		System.out.println("Klasifikazioa era egokian sortzen da?\n------------------------------------- ");
+		                      
+	}
+
+	private static void generadoreSudokuFrogak() {
+		System.out.println("Sudoku berria ausaz eta zailtazunaren arabera sortzen da?\n---------------------------------------------------------");
+	}
+
+	private static void erregistroLehioaFrogak() {
+		System.out.println("Erregistroa ondo dabil?\n-----------------------");
+	}
+
+	private static void aukeratuLeihoaFrogak() {
+		 System.out.println("Aukeratu leihoa ondo dabil?\n---------------------------");
+		
+	}
+
+	private static void adabegiFrogak() {
+		System.out.println("Adabegi klasearen metodoen frogak:\n----------------------------------");
+		adabegiaSortuFrogak();
+		semeaGehituFrogak();
+		semeaEzabatuFrogak();
+	}
+
+	private static void semeaEzabatuFrogak() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void semeaGehituFrogak() {
+		System.out.print("- Semea sortzen da? ");
+		Adabegia<String> ada=new Adabegia<String>("Adabegi berria naiz", 0);
+		Adabegia<String> adaSeme=ada.gehituSemea("Ni semea naiz aldiz", 0);
+		System.out.print(ada.content+",");
+		System.out.println(adaSeme.content);
+		
+	}
+
+	private static void adabegiaSortuFrogak() {
+		System.out.print("- Adabegia berria zara? ");
+		Adabegia<String> ada=new Adabegia<String>("Adabegi berria naiz", 0);
+		System.out.println(ada.content);
 	}
 
 	private static void proba(){
@@ -50,6 +134,8 @@ public class ProbatzekoMain2 {
 		//		s.inprimatuSudokuZuzena();
 
 		s.inprimatuSudoku();
+		System.out.println("\n");
 		//s.inprimatuZuzena();
 	}
+	
 }
